386402aba9a10e2175bacdcdf09d3105c404c278
OronF <oron.fink@gmail.com>
2026-02-26 23:42:26 +0200

    adding the login base page

--- management-app/app/page.tsx
+++ management-app/app/page.tsx
@@ -2,7 +2,7 @@ import Login from "./login/Login";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex h-full w-full min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Login></Login>
     </div>
   );
